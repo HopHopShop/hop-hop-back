6e7e735fa0896ded9a5d53f4ade238bdc7a6530d
LazebnyA <oleksandrlazebnij8@gmail.com>
2024-11-01 18:12:29 +0200

    news-image-link: change link building logic, make link shorter

--- news/models.py
+++ news/models.py
@@ -7,7 +7,7 @@ from pytils.translit import slugify
 
 def news_image_file_path(instance, filename):
     _, extension = os.path.splitext(filename)
-    filename = f"{slugify(instance.title)}-{uuid.uuid4()}{extension}"
+    filename = f"news-{instance.id}-{uuid.uuid4()}{extension}"
     return os.path.join("uploads/news_images/", filename)
 
 
